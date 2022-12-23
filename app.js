0d85a8bdcad68a5bd87cb1e2b9535e8fa6b46772
shoaib171 <shoaibrasheed171@gmail.com>
2022-12-23 13:42:11 -0800

    Notes-app

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 //------To...Do's:
 //Set validate function on  title and note to check validate function
 //Make Extra Button like delete but this button  add functionality to "Edit note"
-//Set scroll bar 
+//Set scroll bar
 
 showNotes();
 
 // If user adds a note, add it to the localStorage
 let addBtn = document.getElementById("addBtn");
-addBtn.addEventListener("click", function(e) {
+addBtn.addEventListener("click", function (e) {
   let addTxt = document.getElementById("addTxt");
   let addTitle = document.getElementById("addTitle");
   let notes = localStorage.getItem("notes");
@@ -18,13 +18,13 @@ addBtn.addEventListener("click", function(e) {
   }
   let myObj = {
     title: addTitle.value,
-    text: addTxt.value
-  }
+    text: addTxt.value,
+  };
   notesObj.push(myObj);
   localStorage.setItem("notes", JSON.stringify(notesObj));
   addTxt.value = "";
   addTitle.value = "";
-//   console.log(notesObj);
+  //   console.log(notesObj);
   showNotes();
 });
 
@@ -37,7 +37,7 @@ function showNotes() {
     notesObj = JSON.parse(notes);
   }
   let html = "";
-  notesObj.forEach(function(element, index) {
+  notesObj.forEach(function (element, index) {
     html += `
             <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
                     <div class="card-body">
@@ -58,7 +58,7 @@ function showNotes() {
 
 // Function to delete a note
 function deleteNote(index) {
-//   console.log("I am deleting", index);
+  //   console.log("I am deleting", index);
 
   let notes = localStorage.getItem("notes");
   if (notes == null) {
@@ -72,22 +72,18 @@ function deleteNote(index) {
   showNotes();
 }
 
-
-let search = document.getElementById('searchTxt');
-search.addEventListener("input", function(){
-
-    let inputVal = search.value.toLowerCase();
-    // console.log('Input event fired!', inputVal);
-    let noteCards = document.getElementsByClassName('noteCard');
-    Array.from(noteCards).forEach(function(element){
-        let cardTxt = element.getElementsByTagName("p")[0].innerText;
-        if(cardTxt.includes(inputVal)){
-            element.style.display = "block";
-        }
-        else{
-            element.style.display = "none";
-        }
-        // console.log(cardTxt);
-    })
-})
-
+let search = document.getElementById("searchTxt");
+search.addEventListener("input", function () {
+  let inputVal = search.value.toLowerCase();
+  // console.log('Input event fired!', inputVal);
+  let noteCards = document.getElementsByClassName("noteCard");
+  Array.from(noteCards).forEach(function (element) {
+    let cardTxt = element.getElementsByTagName("p")[0].innerText;
+    if (cardTxt.includes(inputVal)) {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+    // console.log(cardTxt);
+  });
+});
